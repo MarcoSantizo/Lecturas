597b1fc9a906c93ec9276222bb6ea56d906038c1
Marco Santizo <marcoantoniosantizo@gmail.com>
2026-07-19 10:21:04 -0600

    Corregir ruta del visor PDF.js en GitHub Pages

--- script2.js
+++ script2.js
@@ -119,7 +119,7 @@ function leerPDF(pdf, titulo){
     tituloPDF.textContent = "📖 Leyendo: " + titulo;
 
 
-    lector.src = "pdfjs/web/viewer.html?file=../../" + pdf;
+    lector.src = "pdfjs/web/viewer.html?file=../../Lecturas/" + pdf;
 
 
     visor.style.display = "block";
